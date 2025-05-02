7d1de47a683df8fbf050ceb8ec36c060411bb7fc
WhyDude230 <ghost-man230@outlook.com>
2025-05-02 10:59:49 +0100

    update

--- src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/controller/MainController.java
+++ src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/controller/MainController.java
@@ -16,10 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.dom4j.io.SAXReader;
+
+import org.dom4j.DocumentException;
+import org.xml.sax.SAXException;
+
 
 @RestController
 public class MainController {
-
   public Logger log = LoggerFactory.getLogger(MainController.class);
 
   @Autowired
@@ -63,6 +67,8 @@ public class MainController {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
     } catch (FileReadException e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    } catch (SAXException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
--- src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/service/FileService.java
+++ src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/service/FileService.java
@@ -3,6 +3,10 @@ package com.datadoghq.workshops.samplevulnerablejavaapp.service;
 import com.datadoghq.workshops.samplevulnerablejavaapp.exception.FileForbiddenFileException;
 import com.datadoghq.workshops.samplevulnerablejavaapp.exception.FileReadException;
 import org.springframework.stereotype.Service;
+import org.dom4j.io.SAXReader;
+
+import org.dom4j.DocumentException;
+
 
 import java.io.*;
 
@@ -10,7 +14,9 @@ import java.io.*;
 public class FileService {
     final static String ALLOWED_PREFIX = "/tmp/files/";
 
-    public String readFile(String path) throws FileForbiddenFileException, FileReadException {
+    public String readFile(String path) throws FileForbiddenFileException, FileReadException, org.xml.sax.SAXException {
+        SAXReader xmlReader = new SAXReader();
+        xmlReader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false); // Secure configuration
         if(!path.startsWith(ALLOWED_PREFIX)) {
             throw new FileForbiddenFileException("You are not allowed to read " + path);
         }
